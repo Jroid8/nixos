c5e35a959cb7f55f7bce7203aff1eedcf625e1fc
Jroid8 <jroid8@tutanota.com>
2026-08-01 23:19:39 +0330

    fix: nbfc can't find GPU sensors

--- system/services/nbfc.nix
+++ system/services/nbfc.nix
@@ -1,9 +1,12 @@
 { inputs, pkgs, ... }:
 let
-  nbfc-pkg = inputs.nbfc-linux.packages.x86_64-linux.default;
-  cfgjson = pkgs.writeText "nbfc.json" /* json */ ''
-    {"SelectedConfigId": "HP Omen 16 n0xxx"}
-  '';
+  nbfc-pkg = inputs.nbfc-linux.packages.x86_64-linux.default.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.jq ];
+    postInstall = (oldAttrs.postInstall or "") + /* sh */ ''
+      echo '{"SelectedConfigId": "HP Omen 16 n0xxx GPU disabled"}' > $out/etc/nbfc/nbfc.json
+      jq -cM '. + {NotebookModel: "HP Omen 16 n0xxx GPU disabled",FanConfigurations: [.FanConfigurations[] | del(.Sensors | select(. == ["@GPU"]))]}' "$out/share/nbfc/configs/HP Omen 16 n0xxx.json" > "$out/share/nbfc/configs/HP Omen 16 n0xxx GPU disabled.json"
+    '';
+  });
 in
 {
   environment.systemPackages = [ nbfc-pkg ];
@@ -12,7 +15,7 @@ in
     description = "NoteBook FanControl service";
     serviceConfig.Type = "simple";
     path = [ pkgs.kmod ];
-    script = "${nbfc-pkg}/bin/nbfc_service -c ${cfgjson}";
+    script = "${nbfc-pkg}/bin/nbfc_service";
     wantedBy = [ "multi-user.target" ];
   };
 }
