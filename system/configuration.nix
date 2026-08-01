5964ce7eb9c3769a14472a5b439cf062f355988b
Jroid8 <jroid8@tutanota.com>
2026-08-01 22:49:04 +0330

    configure file systems and swap

--- system/configuration.nix
+++ system/configuration.nix
@@ -26,12 +26,33 @@
   boot.kernelParams = [ "amd_pstate=active" ];
 
   fileSystems = {
-    "/".options = [ "compress=zstd" ];
-    "/nix".options = [
-      "compress=zstd"
-      "noatime"
-    ];
+    "/" = {
+      device = "/dev/disk/by-uuid/093572c0-81dc-40a9-b7b4-1cc86173d2d2";
+      fsType = "btrfs";
+      options = [
+        "subvol=root"
+        "compress=zstd"
+      ];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/093572c0-81dc-40a9-b7b4-1cc86173d2d2";
+      fsType = "btrfs";
+      options = [
+        "subvol=nix"
+        "compress=zstd"
+        "noatime"
+      ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/025F-70BD";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
   };
+  swapDevices = [ { device = "/dev/disk/by-uuid/ffd87ad8-cdd8-4c66-a6f4-8fc3afa63741"; } ];
 
   # GRUB
   boot.loader.grub = {
