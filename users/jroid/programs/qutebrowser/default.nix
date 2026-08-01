1de7a4d9406cd2aa7ed355909163df1a31b6fa0f
Jroid8 <jroid8@tutanota.com>
2026-08-01 23:48:54 +0330

    add nixpkgs and nixos options search shortcut to qutebrowser

--- users/jroid/programs/qutebrowser/default.nix
+++ users/jroid/programs/qutebrowser/default.nix
@@ -18,6 +18,8 @@
       wi = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
       arwi = "https://wiki.archlinux.org/?search={}";
       nixwi = "https://wiki.nixos.org/index.php?search={}";
+      nixpkgs = "https://search.nixos.org/packages?query={}";
+      nixopts = "https://search.nixos.org/options?query={}";
       goo = "https://www.google.com/search?hl=en&q={}";
     };
     extraConfig = /* python */ ''
