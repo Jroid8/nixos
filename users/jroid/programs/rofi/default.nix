cb7bc612823520a1891a996636af2b6dc46ae68c
Jroid8 <jroid8@tutanota.com>
2026-08-02 21:58:56 +0330

    fix: rofi in the global PATH is not the custom one

--- users/jroid/programs/rofi/default.nix
+++ users/jroid/programs/rofi/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }: {
-  custom-pkgs.rofi = pkgs.rofi.override (old: {
+{ pkgs, config, ... }: {
+  custom-pkgs.rofi = pkgs.rofi.override (_: {
     plugins = [ pkgs.rofi-calc ];
     theme = ./mytheme.rasi;
   });
-  home.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
+  home.packages = [ pkgs.nerd-fonts.jetbrains-mono config.custom-pkgs.rofi ];
 }
