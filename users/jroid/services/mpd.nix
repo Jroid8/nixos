2d3692e69b2a87e699ecfd2add4a7a2601debecc
Jroid8 <jroid8@tutanota.com>
2026-08-04 21:57:15 +0330

    use pipewire output for mpd

--- users/jroid/services/mpd.nix
+++ users/jroid/services/mpd.nix
@@ -4,6 +4,12 @@
     enable = true;
     musicDirectory = "${config.home.homeDirectory}/Music";
     playlistDirectory = "${config.xdg.dataHome}/mpd/playlists/";
+    extraConfig = ''
+      audio_output {
+      	type "pipewire"
+      	name "MPD Output"
+      }
+    '';
   };
   systemd.user.services = {
     mps-init = {
