3415a258c08203d73597bed5133c248bab8d8898
Jroid8 <jroid8@tutanota.com>
2026-08-05 16:06:37 +0330

    categorize system packages

--- system/packages.nix
+++ system/packages.nix
@@ -1,24 +1,32 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    android-file-transfer
+    # Commandline Utils
     bash
-    cifs-utils
-    cryptsetup
     curl
     fd
     file
     fish
-    gcc
     lsof
     nix-output-monitor
     ripgrep
-    ungoogled-chromium
     vim
 
+    # Filesystem Utils
+    android-file-transfer
+    cifs-utils
+    cryptsetup
+    rar
+    zip
+
+    # Gui
+    ungoogled-chromium
+
+    # Wine
     wineWow64Packages.waylandFull
     winetricks
 
+    # Look and Feel
     papirus-icon-theme
     liberation_ttf
   ];
