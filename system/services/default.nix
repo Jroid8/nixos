47214d7e0005fcf2c0e4268848c02778088df9db
Jroid8 <jroid8@tutanota.com>
2026-08-13 19:51:34 +0330

    add 32bit alsa support

--- system/services/default.nix
+++ system/services/default.nix
@@ -27,8 +27,11 @@
     };
     pipewire = {
       enable = true;
-      alsa.enable = true;
       pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
     };
   };
 
