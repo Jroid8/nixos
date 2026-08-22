f253d770b7d758fc17500e668d6044ebd9105a8b
Jroid8 <jroid8@tutanota.com>
2026-08-22 23:28:58 +0330

    move nps to its own module

---
+++ modules/nixos/nps.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }: {
+  environment.systemPackages = [ pkgs.nps ];
+  systemd = {
+    services.refresh-nps-cache = {
+      path = [ "/run/current-system/sw/" ];
+      serviceConfig = {
+        Type = "oneshot";
+        User = "jroid";
+      };
+      script = ''
+        set -eu
+        echo "Start refreshing nps cache"
+        ${pkgs.nps}/bin/nps -r -dddd -e
+        echo "finished nps cache with exit code $?."
+      '';
+    };
+    timers = {
+      refresh-nps-cache = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "weekly";
+          Persistent = true;
+          Unit = "refresh-nps-cache.service";
+        };
+      };
+    };
+  };
+}
--- system/services/default.nix
+++ system/services/default.nix
@@ -15,35 +15,6 @@
     };
   };
 
-  systemd = {
-    services = {
-      refresh-nps-cache = {
-        path = [ "/run/current-system/sw/" ];
-        serviceConfig = {
-          Type = "oneshot";
-          User = "jroid";
-        };
-        script = ''
-          set -eu
-          echo "Start refreshing nps cache"
-          ${pkgs.nps}/bin/nps -r -dddd -e
-          echo "finished nps cache with exit code $?."
-        '';
-      };
-      tor.wantedBy = lib.mkForce [ ];
-    };
-    timers = {
-      refresh-nps-cache = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnCalendar = "weekly";
-          Persistent = true;
-          Unit = "refresh-nps-cache.service";
-        };
-      };
-    };
-  };
-
   nix.gc = {
     automatic = true;
     dates = "weekly";
