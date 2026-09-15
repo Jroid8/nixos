1b65473f6ad0570adb0611866865d2ad38cdd2e1
Jroid8 <jroid8@tutanota.com>
2026-09-15 17:23:23 +0330

    fix: use absolute paths for modules in gen-hosts

--- lib/gen-hosts.nix
+++ lib/gen-hosts.nix
@@ -6,14 +6,15 @@ let
     hostName:
     (
       let
-        hostPath = "../hosts/${hostName}";
-        system = builtins.readFile "${hostPath}/system.txt" |> lib.strings.trim;
+        hostPath = ./.. + "/hosts/${hostName}";
+        system = builtins.readFile (hostPath + "/system.txt") |> lib.strings.trim;
         users =
           builtins.readDir hostPath
           |> lib.attrsets.filterAttrs (_: t: t == "directory")
           |> builtins.attrNames
-          |> genAttrs (user: "${hostPath}/${user}/home.nix")
-          |> lib.attrsets.filterAttrs (_: builtins.pathExists);
+          |> genAttrs (user: hostPath + "/${user}/home.nix")
+          |> lib.attrsets.filterAttrs (_: builtins.pathExists)
+          |> lib.attrsets.mapAttrs (p: ./. + p);
         mypkgs = import ./my-packages.nix;
         specialArgs = {
           inherit inputs mypkgs;
@@ -23,7 +24,7 @@ let
         inherit system specialArgs;
         modules = [
           (inputs.import-tree ../modules/nixos)
-          "${hostPath}/system/configuration.nix"
+          (hostPath + "/system/configuration.nix")
           inputs.nix-index-database.nixosModules.default
           inputs.nur.modules.nixos.default
           inputs.home-manager.nixosModules.home-manager
@@ -42,7 +43,7 @@ let
       }
     );
 in
-builtins.readDir ./hosts
+builtins.readDir ../hosts
 |> lib.attrsets.filterAttrs (_: t: t == "directory")
 |> builtins.attrNames
 |> genAttrs mkHost
